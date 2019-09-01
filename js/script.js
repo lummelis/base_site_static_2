f3dfecfe4b46fb333a7d098e763cb436773d89cf
lummelis <lummelis@hawaii.edu>
2019-09-01 12:39:12 -1000

    edit in readme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+    //TOGGLE MENU NAV
+    var menu = document.getElementsByClassName("menu");
+    var i;
+    for (i = 0; i < menu.length; i++) {
+        menu[i].onclick = function(){
+            this.classList.toggle("expanded");
+        }
+    }
